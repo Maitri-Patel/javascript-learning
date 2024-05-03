2d1c7fd647180b882064ea0395cbef2b52e81ef7
Maitri-Patel <mpatel18@academic.rrc.ca>
2024-05-03 11:58:40 -0500

    Intro of variables

---
+++ index.js
@@ -0,0 +1,44 @@
+// variable - A container that stores a value 
+//            behaves as if it were the value it contains 
+
+// 1. declaratio - let x;
+// 2. assignment - x = 10;
+// 3. both let x = 10;
+
+let age = 20;
+let price = 15.22;
+let gpa = 3.76;
+
+console.log(`Your age is ${age}`);
+console.log(`The price of book is ${price}`);
+console.log(`My current gpa is ${gpa}`);
+
+// typeof function is used to know the type of variable 
+console.log (typeof age);
+
+// boolean 
+let online = true;
+console.log(typeof online);
+console.log(`are you available ${online}`);
+
+let name = "Maitri";
+document.getElementById("myH1").textContent = `Welcome to JavaScript Learning ${name}`
+
+
+
+
+/* let fullname = "Maitri Patel"
+let age = "20";
+let student = "true";
+
+document.getElementById("p1").textContent = `Your name is ${fullname}`;
+document.getElementById("p2").textContent = `Your age is ${age}`;
+document.getElementById("p3").textContent = `You are a ${student}`;
+
+console.log(typeof fullname);
+
+let result = 6/2 ** (2+5);
+console.log(result) */
+
+/* let username
+document.getElementById("myH1").textContent = `Hello, ${username} `; */
